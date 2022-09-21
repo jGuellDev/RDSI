68da3dd399aeaecf5377f2e71870361219746c3a
jlguell <jlguell@uninorte.edu.co>
2022-09-21 13:43:41 -0500

    v1 s Please enter the commit message for your changes. Lines starting

---
+++ Js/rds.js
@@ -0,0 +1,12 @@
+
+const CABECERA = "BIENVENIDO A RED SOCIAL DE IMAGENES"
+alert(CABECERA)
+
+
+
+/* Funcion para iniciar un evento, puede ser en el load del boddy */
+function iniciar() {
+    setTimeout(() => {
+        open("https://www.google.com/");
+    }, 2000)
+}
